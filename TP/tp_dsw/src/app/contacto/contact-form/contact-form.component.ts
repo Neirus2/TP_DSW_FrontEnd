850552a2f27d848f98212795f1b6c22b07599f86
unknown <soolaltair@gmail.com>
2023-09-19 17:25:55 -0300

    lógica del formulario contact

--- TP/tp_dsw/src/app/contacto/contact-form/contact-form.component.ts
+++ TP/tp_dsw/src/app/contacto/contact-form/contact-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-contact-form',
@@ -12,9 +14,19 @@ export class ContactFormComponent {
     message: ''
   };
 
+  constructor(private http: HttpClient) { } // Inyecta HttpClient en el constructor
+
   submitForm() {
-    // Aquí puedes agregar la lógica para enviar el formulario
-    console.log('Formulario enviado:', this.formData);
+    // Realiza una solicitud HTTP POST al servidor para enviar el formulario
+    this.http.post('http://localhost:3000/enviar-correo', this.formData).subscribe(
+      (response) => {
+        console.log('Formulario enviado:', this.formData);
+        // Aquí puedes manejar la respuesta del servidor, como mostrar un mensaje de éxito al usuario.
+      },
+      (error) => {
+        console.error('Error al enviar el formulario:', error);
+        // Aquí puedes manejar cualquier error que ocurra al enviar el formulario.
+      }
+    );
   }
-  
 }
